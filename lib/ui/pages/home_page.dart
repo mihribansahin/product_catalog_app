419bcbe847fd2842a93468252c5f660d5b7b06f3
mihribansahin <mishsapp@gmail.com>
2022-04-13 21:25:14 +0300

    added plugins & created folders

---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
